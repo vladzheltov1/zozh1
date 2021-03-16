271957cf3a9a795707aacf3edf63c95366b67369
vladzheltov1 <blog.neprogg@gmail.com>
2021-03-16 16:31:16 +0300

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const router = require('./modules/router');
 // ------------------------------------------//
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 const app = express();
