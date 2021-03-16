01d7a13c15726692e20d5ad07fca8d696853c5b5
vladzheltov1 <blog.neprogg@gmail.com>
2021-03-16 22:30:03 +0300

    git push origin

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const router = require('./modules/router');
 // ------------------------------------------//
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.APP_PORT || 3001;
+const IP   = process.env.APP_IP || process.env.APP_IP + 1;
 
 
 const app = express();
@@ -31,17 +32,17 @@ app.use('/public', express.static('public'));
 app.use(cookieParser());
 app.use(router);
 
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: "supersecretwordthatnooneknows"
-}));
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     secret: "supersecretwordthatnooneknows"
+// }));
 
 
 function start() {
     try{
-        app.listen(PORT, () => {
-            console.log("Server has been started on port: " + PORT);
+        app.listen(PORT, IP, () => {
+            console.log(`Server has been started on IP ${IP} and port ${PORT}`);
         });
     }
     catch(e){
